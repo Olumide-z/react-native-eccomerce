ea86ffb8e9337becfda5747fbb6b6123ee6533f3
Olumide llasanmi <Olumide.llasanmi@ss-limited.com>
2026-01-04 11:31:19 +0100

    feat: auth setup[C

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,20 @@
+import path from "path";
 import express from "express";
-import { ENV } from "./config/env";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+
+import { serve } from "inngest/express";
+import { functions, inngest } from "./config/ingest.js";
 
 const app = express();
 
 const __dirname = path.resolve();
+
+app.use(express.json());
+// adds auth object under the req
+app.use(clerkMiddleware());
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 // make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
@@ -14,4 +25,11 @@ if (ENV.NODE_ENV === "production") {
   });
 }
 
-app.listen(ENV.PORT, () => console.log(`Server running on port ${ENV.PORT}`));
+const startServer = () => {
+  connectDB();
+  app.listen(ENV.PORT, () => {
+    console.log(`Server running on port ${ENV.PORT}`);
+  });
+};
+
+startServer();
